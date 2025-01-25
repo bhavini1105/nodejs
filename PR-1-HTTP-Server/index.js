e1f1948378e7c2a2ed201709d3f6c8fb263af23f
bhavini1105 <bhavinipatel7532@gmail.com>
2025-01-25 19:27:28 +0530

    HTTP-Server

---
+++ PR-1-HTTP-Server/index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const handleRequest = require('./Controller/serverController')
+const port = 8010;
+
+// const handleRequest = (req, res) => {
+//     res.write("<h1>hello</h1>");
+//     res.end();
+// }
+
+const server = http.createServer(handleRequest);
+
+server.listen(port, (err) => {
+
+    if (err) {
+        console.log("Server is Not Working.....");
+    }
+    else {
+        console.log("Server is Working.....");
+        console.log("http://localhost:" + port)
+    }
+})
